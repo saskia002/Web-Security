aa0d5f0dd7010cf7e74f10744c022b422aa07f8e
Saskia <90192374+saskia002@users.noreply.github.com>
2025-09-07 14:19:01 +0300

    1st homework

---
+++ 1/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//session_name("Saskia");
+
+//session_set_cookie_params([
+//	"path"     => "/",
+//	"secure"   => "true",
+//	"samesite" => "Strict",
+//	"httponly" => true,
+//]);
+
+//session_start();
+
+
+header_remove();
+header("Strict-Transport-Security: max-age=15552000;  includeSubDomains; preload");
