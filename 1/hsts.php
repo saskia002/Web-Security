4443e6532746c5b9a54aac2a5d228ecc12aa789a
Saskia <90192374+saskia002@users.noreply.github.com>
2025-09-07 16:13:09 +0300

    1st

--- 1/hsts.php
+++ 1/hsts.php
@@ -2,3 +2,10 @@
 
 header_remove();
 header("Strict-Transport-Security: max-age=15552000;  includeSubDomains");
+
+
+$headers = headers_list();
+
+foreach ($headers as $header) {
+	echo "$header <br>";
+}
