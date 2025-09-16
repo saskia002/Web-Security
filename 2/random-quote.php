d1b601bfe3339054473c63e723eec843170cc2b3
Saskia <90192374+saskia002@users.noreply.github.com>
2025-09-16 16:24:30 +0300

    Create random-quote.php

---
+++ 2/random-quote.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Random quote</title>
+</head>
+<body>
+
+	<iframe src="https://websec.ee/sop/random_quote.php" frameborder="0"></iframe>
+</body>
+</html>
